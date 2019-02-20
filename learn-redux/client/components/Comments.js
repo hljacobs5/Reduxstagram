b97789e9045d63afe7b939c8f10fb8e98d18bc8f
Haley Jacobs <haleyljacobs@gmail.com>
2019-02-20 10:23:44 -0700

    Fixed Comments.js

--- learn-redux/client/components/Comments.js
+++ learn-redux/client/components/Comments.js
@@ -1,10 +1,34 @@
 import React from 'react';
 
 const Comments = React.createClass({
-	render( {
+	renderComment(comment, i) {
 		return (
-			<div className='comment'>
-				I'm the comments!!!
+			<div className='comment' key={i}>
+				<p>
+					<strong>{comment.user}</strong>
+					{comment.text}
+					<button className='remove-comment'>&times;</button>
+				</p>
+			</div>
+		)	
+	},
+	handleSubmit(e) {
+		e.preventDefault();
+		const { postId } = this.props.params;
+		const author = this.refs.author.value;
+		const comment = this.refs.comment.value;
+		this.props.addComment(postId, author, comment);
+	}, 
+
+	render() {
+		return (
+			<div className='comments'>
+				{this.props.postComment.map(this.renderComment)}
+				<form ref='commentForm' className='comment-form'>
+					<input type='text' ref='author' placeholder='author'/>
+					<input type='text' ref='comment' placeholder='comment'/>
+					<input type='submit' hidden />
+				</form>
 			</div>
 		)
 	})
--- learn-redux/client/components/Single.js
+++ learn-redux/client/components/Single.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import Photo from './Photo';
 
 const Single = React.createClass({
 	render() {
+		const { postId } = this.props.params;
+
+		const i = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
+		const post = this.props.posts[i];
+
+		const postComments this.props.comments[]
+
 		return (
 			<div className='single-photo'>
-			I'm the single
+				<Photo i={i} post={post} {...this.props} />
+				<Comments postComments={postComments} />
 			</div>
 		)
 	}
