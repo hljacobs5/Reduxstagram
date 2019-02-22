5a5a1149606a6316a2d768c3e58de8f855b74310
Haley Jacobs <haleyljacobs@gmail.com>
2019-02-22 10:02:07 -0700

    Added Redux Reducer Composition

--- learn-redux/client/reducers/comments.js
+++ learn-redux/client/reducers/comments.js
@@ -2,4 +2,28 @@ function comments(state=[], action) {
 	return state;
 }
 
+function postComments(state = [], action) {
+	switch(action.type){
+		case 'ADD_COMMENT':
+			return [...state,{
+				user: action.author,
+				text: action.comment
+			}];
+		case 'REMOVE_COMMENT':
+			return state;
+		default:
+			return state;
+	}
+	return state;
+}
+
+function comments(state=[], action) {
+	if(typeof action.postId !== 'undefined') {
+		return {
+			...state,
+			[action.postId]: postcomments(state[action.postId], action)
+		}
+	}
+	return state;
+}
 export default comments;
